fcb12eb36bf661e12f31d602172bf592f090f2a1
fahmianshar6 <fahmipc683@gmail.com>
2025-09-09 18:36:46 +0800

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,14 @@
+public class day2 {
+    public static void main(String[] args) {
+        System.out.print("fahmi");
+        System.out.println();
+        System.out.println("fahmi anshar");
+
+        String nama = "fahmi";
+        String alaamt = "banua";
+
+        System.out.printf("nama saya %s asal %S.",nama,alaamt);
+
+
+    }
+}
