64eb8607c8963aa87fb618a0bb869202d3911a00
fahmianshar6 <fahmipc683@gmail.com>
2025-09-10 23:10:47 +0800

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,11 @@
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println("memahami komen");
+        //komen multi line coment
+        /* koment 1 single line */
+        /** documentasion comment**/
+
+    }
+}
+
+
