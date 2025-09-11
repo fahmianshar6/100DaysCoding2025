7781c73693d9005c7833c3d98666c452115fe5d1
fahmianshar6 <fahmipc683@gmail.com>
2025-09-11 20:56:02 +0800

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,13 @@
+public class day4 {
+    public static void main(String[] args) {
+        System.out.println("Halo\nwelcome bro");
+        // \n membuat baris baru
+        System.out.println("Nama:\tfahmi anshar");
+        System.out.println("Kelas:\tC2024");
+        // \t memberi spasi atau tab
+        System.out.println("emang iye bro?:\"kelasss king\"");
+        // \" menampilkan tanda kutip
+        System.out.println("Path file: C:\\Users\\fahmi\\Documents");
+        // \\ menampilkan backslash
+    }
+}
