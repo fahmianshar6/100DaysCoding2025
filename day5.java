9320384cc364e401c03365bf10d8e6673126687f
fahmianshar6 <fahmipc683@gmail.com>
2025-09-12 23:19:25 +0800

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,15 @@
+public class day5 {
+    public static void main(String[] args) {
+     int umur = 20;
+        System.out.println(umur);
+
+        long jumlahikan = 2000;
+        System.out.println(jumlahikan);
+
+        short jumlahsapi = 200;
+        System.out.println(jumlahsapi);
+
+        byte jumlahayam = 23;
+        System.out.println(jumlahayam);
+    }
+}
