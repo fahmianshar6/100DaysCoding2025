72fe501d68237b4a3dbd543e61a29b0a1db412cb
fahmianshar6 <fahmipc683@gmail.com>
2025-09-13 22:58:15 +0800

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,9 @@
+public class day6 {
+    public static void main(String[] args) {
+        float angka1 = 23.5f;
+        System.out.println(angka1);
+
+        double angka2 = 245.576;
+        System.out.println(angka2);
+    }
+}
