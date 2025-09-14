478395e4708874581e176f7de9d66c874399fbfc
fahmianshar6 <fahmipc683@gmail.com>
2025-09-14 23:53:54 +0800

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,8 @@
+public class day7 {
+    public static void main(String[] args) {
+        char kiw = 'a';
+        System.out.println(kiw);
+        boolean anjay = true;
+        System.out.println(anjay);
+    }
+}
