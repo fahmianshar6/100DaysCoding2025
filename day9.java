11486662e5dbdaef4308eb4ee5adb5367cc7f413
fahmianshar6 <fahmipc683@gmail.com>
2025-09-16 23:43:23 +0800

    Add files via upload

---
+++ day9.java
@@ -0,0 +1,10 @@
+public class day9 {
+    public static void main(String[] args) {
+        int a = 95;
+        System.out.println("sebelum di update"+a);
+        a = 10;
+        System.out.println("setelah di uptdate"+a);
+
+
+    }
+}
