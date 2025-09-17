c528c272ec7e3e8ac5b96bbf06528e7166f4db3e
fahmianshar6 <fahmipc683@gmail.com>
2025-09-17 20:02:08 +0800

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,10 @@
+public class day10 {
+    public static void main(String[] args) {
+   final double pi = 3.14;
+   final int jumlah = 100;
+        System.out.println(pi);
+        System.out.println(jumlah);
+   // nilai final tidak bisa di update dia sifatnya mutlak
+
+    }
+}
