3c850f14663d67be87b0b02eae75173d2f5388e6
fahmianshar6 <fahmipc683@gmail.com>
2025-09-18 20:59:59 +0800

    Add files via upload

---
+++ day11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class day11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nama:");
+        String nama = sc.nextLine();
+        System.out.println(nama);
+        System.out.println("umur saya:");
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
