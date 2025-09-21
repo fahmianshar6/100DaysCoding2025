22dccabb9658d912665ed03d542e542e85001235
fahmianshar6 <fahmipc683@gmail.com>
2025-09-21 23:14:03 +0800

    Add files via upload

---
+++ day14.java
@@ -0,0 +1,7 @@
+public class day14 {
+    public static void main(String[] args) {
+        String aint = "12";
+        int conver = Integer.parseInt(aint);
+        System.out.println(conver);
+    }
+}
