0fa684ebd8cda8a00aeb8d7a50a77c2359da0655
fahmianshar6 <fahmipc683@gmail.com>
2025-09-22 23:56:15 +0800

    Add files via upload

---
+++ day15.java
@@ -0,0 +1,10 @@
+public class day15 {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = 10;
+        int penjumlahan = a + b;
+        int pengurangan = a - b;
+        System.out.println("pengurangan :" + pengurangan);
+        System.out.println("penjumlahan :" + penjumlahan);
+    }
+}
