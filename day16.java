a5989873c71e0adbd5548238ed6d8cf298ccc742
fahmianshar6 <fahmipc683@gmail.com>
2025-09-23 23:42:14 +0800

    Add files via upload

---
+++ day16.java
@@ -0,0 +1,10 @@
+public class day16 {
+    public static void main(String[] args) {
+        double angka1 = 30;
+        double angka2 = 20;
+
+        // Menampilkan hasil
+        System.out.println("Hasil Perkalian: " + angka1 * angka2);
+        System.out.println("Hasil Pembagian: " + angka1 / angka2);
+    }
+}
