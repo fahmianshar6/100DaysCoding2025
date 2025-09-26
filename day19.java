8dfa1ca4f535fbfb9c3f898f02ac54c78674145c
fahmianshar6 <fahmipc683@gmail.com>
2025-09-26 23:27:30 +0800

    Add files via upload

---
+++ day19.java
@@ -0,0 +1,14 @@
+public class day19 {
+    public static void main(String[] args) {
+        // Tipe data besar
+        double nilaiDouble = 99.75;
+        int nilaiInt;
+
+        // Konversi manual
+        nilaiInt = (int) nilaiDouble;
+
+        // Cetak hasil konversi
+        System.out.println("Nilai double : " + nilaiDouble);
+        System.out.println("Nilai int    : " + nilaiInt);
+    }
+}
