2a64e9acfacc57fd29e3b82ab862f78d5c8682ce
fahmianshar6 <fahmipc683@gmail.com>
2025-09-27 23:04:56 +0800

    Add files via upload

---
+++ day20.java
@@ -0,0 +1,15 @@
+public class day20 {
+    public static void main(String[] args) {
+     int fahmi = 12;
+     double fahmi1 = 12.4;
+     char fahmi2 = 'c';
+
+     String st1 = Integer.toString(fahmi);
+     String st2 = Double.toString(fahmi1);
+     String st3 = Character.toString(fahmi2);
+
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+    }
+}
