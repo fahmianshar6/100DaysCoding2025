21ee641c089560b294c70720ab13635a6f9aa798
fahmianshar6 <fahmipc683@gmail.com>
2025-09-29 22:59:53 +0800

    Add files via upload

---
+++ day22.java
@@ -0,0 +1,8 @@
+public class day22 {
+    public static void main(String[] args) {
+        int a = 4;
+        int hasil = a * a;
+        System.out.println(a);
+        System.out.println(hasil);
+    }
+}
