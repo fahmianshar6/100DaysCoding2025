a3dcafabc2bd893325950bb8474a4fa14e1cf2b0
fahmianshar6 <fahmipc683@gmail.com>
2025-09-30 23:11:15 +0800

    Add files via upload

---
+++ day23.java
@@ -0,0 +1,11 @@
+public class day23 {
+    public static void main(String[] args) {
+        int panjang = 10;
+        int lebar = 5;
+       int luaspersegi = panjang * lebar;
+        System.out.println(panjang);
+        System.out.println(lebar);
+        System.out.println(luaspersegi);
+    }
+}
+
