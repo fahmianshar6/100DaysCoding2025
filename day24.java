961a1d326c51cd14d41fa3b090a82486a5b358ff
fahmianshar6 <fahmipc683@gmail.com>
2025-10-01 23:08:33 +0800

    Add files via upload

---
+++ day24.java
@@ -0,0 +1,7 @@
+public class day24 {
+    public static void main(String[] args) {
+        int r = 5;
+        double PI = 3.14;
+        System.out.println("luas lingkaran"+(PI * r * r));
+    }
+}
