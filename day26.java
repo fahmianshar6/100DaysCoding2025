4864ecf486fc5e22fa29d1e78bb99596f11f7e6b
fahmianshar6 <fahmipc683@gmail.com>
2025-10-03 23:41:34 +0800

    Add files via upload

---
+++ day26.java
@@ -0,0 +1,21 @@
+public class day26 {
+    public static void main(String[] args) {
+     int a = 20;
+        System.out.println("nilai awal a :"+a);
+
+        a += 2;
+        System.out.println(a);
+
+        a -= 4;
+        System.out.println(a);
+
+        a /= 2;
+        System.out.println(a);
+
+        a *= 2;
+        System.out.println(a);
+
+        a %= 2;
+        System.out.println(a);
+    }
+}
