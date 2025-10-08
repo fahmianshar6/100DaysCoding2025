476a2ecbe789a2d716c7be82e128690fae9b1a18
fahmianshar6 <fahmipc683@gmail.com>
2025-10-08 23:06:14 +0800

    Add files via upload

---
+++ day31.java
@@ -0,0 +1,10 @@
+public class day31 {
+    public static void main(String[] args) {
+      boolean a = true;
+      boolean b = false;
+      boolean c = true;
+
+        System.out.println(a && c);
+        System.out.println(a && b);
+    }  
+}
