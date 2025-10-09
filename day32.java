a8dffd7b7542f58938046405baa61ecd4ce7897c
fahmianshar6 <fahmipc683@gmail.com>
2025-10-09 22:25:08 +0800

    Add files via upload

---
+++ day32.java
@@ -0,0 +1,9 @@
+public class day32 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println(a < 9 || b < 9 );
+        System.out.println(a > 2 || b < 2);
+        System.out.println(a > 2 || b > 2);
+    }
+}
