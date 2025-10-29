3a248d02cdb1034300af10c43ef67aa6606acd48
fahmianshar6 <fahmipc683@gmail.com>
2025-10-29 23:24:24 +0800

    Add files via upload

---
+++ day52.java
@@ -0,0 +1,9 @@
+public class day52 {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 10){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
