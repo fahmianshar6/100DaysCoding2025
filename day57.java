a977b6b6b45ab3ac295de1302f2a91a27af79d23
fahmianshar6 <fahmipc683@gmail.com>
2025-11-03 22:48:37 +0800

    day57.java

--- day57.java
+++ day57.java
@@ -1,10 +1,11 @@
 public class day57 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5 ; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 1; i <= 5 ; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("x");
             }
             System.out.println();
         }
     }
 }
+
