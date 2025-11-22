49f385f8a8d2973082c9529e870dec6c0a3cd17e
fahmianshar6 <fahmipc683@gmail.com>
2025-11-22 22:05:32 +0800

    Add files via upload

---
+++ days76.java
@@ -0,0 +1,13 @@
+public class days76 {
+    public static void main(String[] args) {
+String a = "Fahmi";
+String b = "Fahmi anshar";
+String c = "ep ep";
+
+        System.out.println("cek equals :"+a.equals("Fahmi"));
+        System.out.println("cek equals ignore case :"+b.equalsIgnoreCase("fahmi anshar"));
+        System.out.println("cek contains :"+b.contains("Fahmi"));
+        System.out.println("cek is empty :"+c.isEmpty());
+
+    }
+}
