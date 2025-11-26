990d8f0a36ae5d121fbe71021121652d93fe085d
fahmianshar6 <fahmipc683@gmail.com>
2025-11-26 22:41:01 +0800

    day 80

---
+++ day80.java
@@ -0,0 +1,11 @@
+public class day80 {
+    public static void main(String[] args) {
+        //array 1 dimensi yang isinya 3 angka
+        int[] angka = {5, 10, 15};
+
+        System.out.println("Array 1 dimensi:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Index " + i + " = " + angka[i]);
+        }
+    }
+}
