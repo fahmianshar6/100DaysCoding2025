90c4243e67edf0c9d4a9ee77c4000556a7b66853
fahmianshar6 <fahmipc683@gmail.com>
2025-11-27 23:10:31 +0800

    Add files via upload

---
+++ day81.java
@@ -0,0 +1,8 @@
+public class day81 {
+    public static void main(String[] args) {
+     int [] a = {12,34,67,};
+        System.out.println("elemen index 0 :"+a[0]);
+        System.out.println("elemen index 1 :"+a[1]);
+        System.out.println("elemen index 2 :"+a[2]);
+    }
+}
